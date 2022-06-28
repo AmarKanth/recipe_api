d64e8e211c8fdbc9735e7ee72c2badda421ab325
Amar Kanth <amarkanth@Amars-MacBook-Pro.local>
2022-06-28 13:08:31 +0530

    Added Logout and LogoutAll views

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView, TokenBlacklistView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from authentication.views import LogoutView, LogoutAllView
 
 
 urlpatterns = [
     path('token/create/', TokenObtainPairView.as_view(), name='token_create'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/blacklist/', TokenBlacklistView.as_view(), name='token_balcklist'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout_all/', LogoutAllView.as_view(), name='logout_all'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,29 @@
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-class Logout(APIView):
-	pass
+class LogoutView(APIView):
+	
+	def post(self, request):
+		try:
+			refresh_token = request.data["refresh_token"]
+			token = RefreshToken(refresh_token)
+			token.blacklist()
+			return Response(status=status.HTTP_205_RESET_CONTENT)
+		except Exception as e:
+			return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class LogoutAllView(APIView):
+
+	def post(self, request):
+		tokens = OutstandingToken.objects.filter(user=request.user)
+
+		for token in tokens:
+			BlacklistedToken.objects.get_or_create(token=token)
+
+		return Response(status=status.HTTP_205_RESET_CONTENT)
